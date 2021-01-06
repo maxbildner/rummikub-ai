6d0431d6a64432a66148649018a1a9f68beaa1f3
mbildner <mbildner@icloud.com>
2021-01-06 12:54:21 -0500

    update players and currentPlayer reducers

--- src/reducers/playersReducer.js
+++ src/reducers/playersReducer.js
@@ -1,14 +1,43 @@
 // import { merge } from 'lodash';
 
 
+// EXAMPLE REDUX STORE
+// {
+//    players: {
+//      player1: { 10, 11, ... }    SET (nums refer to tile IDs)
+//      ai:      { 13, 14, ... }    SET
+//    },
+//
+//    board: {
+//      runs: {
+//        0: [ 0, 1, 2, ... ],      ARRAY (nums refer to tile IDs)
+//        1: [ 3, 4, 5, ... ],      ARRAY (nums refer to tile IDs)
+//      }
+//      groups: { 
+//        0: [ 6, 19, 33, ... ],    ARRAY (nums refer to tile IDs)
+//      },
+//    },
+//
+//    boughtIn: {
+//      player1: false,             BOOLEAN
+//      ai: false,                  BOOLEAN
+//    },
+//    
+//    currentPlayer: 'player1'      STRING (also can be null or 'ai'),
+//    
+//    timeRemaining:                DATEOBJ
+//
+// }
+
+
 // prepopulated players slice of store
 const predefinedState = {
-  player1: {},
-  ai: {}
+  player1: new Set(),
+  ai: new Set()
 };
 
 
-const players = (oldState=predefinedState, action) => {
+const players=(oldState=predefinedState, action) => {
   
   // prevent us from accidentally mutating state
   Object.freeze(oldState);
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import players from './playersReducer';
+import currentPlayer from './currentPlayerReducer';
 
 
 const rootReducer = combineReducers({
   players,
+  currentPlayer,
 });
 
 export default rootReducer;
@@ -12,7 +14,27 @@ export default rootReducer;
 // EXAMPLE REDUX STORE
 // {
 //    players: {
-//      player1:
-//      ai: 
-//    }
+//      player1: { 10, 11, ... }    SET (nums refer to tile IDs)
+//      ai:      { 13, 14, ... }    SET
+//    },
+//
+//    board: {
+//      runs: {
+//        0: [ 0, 1, 2, ... ],      ARRAY (nums refer to tile IDs)
+//        1: [ 3, 4, 5, ... ],      ARRAY (nums refer to tile IDs)
+//      }
+//      groups: { 
+//        0: [ 6, 19, 33, ... ],    ARRAY (nums refer to tile IDs)
+//      },
+//    },
+//
+//    boughtIn: {
+//      player1: false,             BOOLEAN
+//      ai: false,                  BOOLEAN
+//    },
+//    
+//    currentPlayer: 'player1'      STRING (also can be null or 'ai'),
+//    
+//    timeRemaining:                DATEOBJ
+//
 // }
